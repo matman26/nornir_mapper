ac49e89b141c93080fe3e046c87941512bda90f4
Matheus Augusto da Silva <a.matheus26@hotmail.com>
2024-06-29 09:30:46 +0100

    N2G Support for diagramming

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from nornir_csv.plugins.inventory import CsvInventory
+from nornir_utils.plugins.functions import print_result
 from plugins.tasks.cdp_map import map_neighbors
 from nornir.core.inventory import Host
-from nornir_utils.plugins.functions import print_result
-from nornir.core.plugins.inventory import InventoryPluginRegister
+from N2G import drawio_diagram 
 from nornir import InitNornir
 from tqdm import tqdm
 
-InventoryPluginRegister.register("CsvInventoryPlugin", CsvInventory)
-
-nr = InitNornir(inventory={'plugin': 'CsvInventoryPlugin', 'options': {'inventory_dir_path' : './inventory/'} },
-                runner={'plugin': 'threaded','options': {'num_workers': 20}})
+nr = InitNornir(
+  inventory={
+    'plugin': 'SimpleInventory', 
+    'options': {
+       'host_file' : './inventory/hosts.yaml'
+    } 
+  },
+  runner={
+    'plugin': 'threaded',
+    'options': {
+       'num_workers': 20
+    }
+  }
+)
 
 current_inventory_len = len(nr.inventory.hosts)
 new_inventory_len = 0
@@ -17,27 +26,55 @@ new_inventory_len = 0
 while current_inventory_len != new_inventory_len:
     current_inventory_len = len(nr.inventory.hosts)
     stage = 1
-    results = nr.run(
-        task=map_neighbors)
+    results = nr.run(task=map_neighbors)
 
     for host, result in tqdm(results.items(), desc=f"Mapping Topology"):
         for neighbor, data in result.result.items():
             shortname=neighbor.split('.')[0]
+            # Populate the neighbor we just find in the inventory so that
+            # we include that neighbor next iteration
             if shortname not in nr.inventory.hosts:
                 nr.inventory.hosts[shortname] = Host(name=shortname, platform='cisco_ios',
                                                     hostname=data['neighbor_address'],
                                                     username='cisco', password='cisco',
                                                     port=22)
-            else:
-                pass
+
+            # We also want to keep track of links to generate the diagram
+            if 'neighbors' not in nr.inventory.hosts[shortname].data:
+                 nr.inventory.hosts[shortname].data['neighbors'] = {}
+
+	    # remote-local  are inverted because we are populating them from the
+	    # neighbors perspective and not the current hosts perspective
+            if host not in nr.inventory.hosts[shortname].data['neighbors']:
+               nr.inventory.hosts[shortname].data['neighbors'][host] = {
+                  'local-interface'  : data['remote_interface'],
+  	          'remote-interface' : data['local_interface'],
+	       }
+
     stage = stage + 1
     new_inventory_len = len(nr.inventory.hosts)
 
 print(f"Done Mapping Topology. Found {new_inventory_len} devices in total.")
-CsvInventory.write(dest_file='inventory/hosts.csv',inventory=nr.inventory)
 print("Inventory Updated")
+print_result(results)
 
+# Lets build a graph now!
+diagram   = drawio_diagram() 
+diag_name = "nornir_mapper_graph.drawio"
 
+print(f"Building diagram...")
+# This inserts a host and each of its N links.
+inventory_list = []
+for hostname, host in nr.inventory.hosts.items():
+  for neighbor, info in host.data.get('neighbors', {}).items():
+    inventory_list.append({
+      "source"    : hostname,
+      "src_label" : info['local-interface'],
+      "target"    : neighbor,
+      "trgt_label": info['remote-interface'],
 
-#print_result(results)
+    })
 
+diagram.from_list(inventory_list)
+diagram.dump_file(filename=diag_name, folder="./")
+print(f"Done building diagram: {diag_name}")
--- plugins/tasks/cdp_map.py
+++ plugins/tasks/cdp_map.py
@@ -1,41 +1,44 @@
 from nornir.core.task import Task, Result
 from ttp import ttp
 
-cdp_template = """
-<vars>
-INTF = "[A-Z][a-zA-Z]+\s[/0-9]+"
-</vars>
-<group name="{{device_name}}">
-{{ device_name }} {{ local_interface | re("INTF")}} {{ holdtime}} {{ capability_string }} {{ platform | re("WORD") }} {{ port_id | re("INTF")}}
-</group>
-"""
-
 cdp_entry_template = """
+<doc>
+-------------------------
+Device ID: R2.lab.local
+Entry address(es):
+  IP address: 2.2.2.2
+Platform: Cisco 3725,  Capabilities: Router Switch IGMP
+Interface: FastEthernet0/1,  Port ID (outgoing port): FastEthernet0/0
+Holdtime : 126 sec
+
+Version :
+Cisco IOS Software, 3700 Software (C3725-ADVENTERPRISEK9-M), Version 12.4(15)T7, RELEASE SOFTWARE (fc3)
+Technical Support: http://www.cisco.com/techsupport
+Copyright (c) 1986-2008 by Cisco Systems, Inc.
+Compiled Wed 13-Aug-08 21:36 by prod_rel_team
+
+advertisement version: 2
+VTP Management Domain: ''
+Duplex: half
+</doc>
+
 <group name="{{neighbor_name}}">
 Device ID: {{ neighbor_name }}
   IP address: {{ neighbor_address }}
 Platform: {{ platform_string | ORPHRASE }},  Capabilities: {{ device_capabilities | ORPHRASE }}
+Interface: {{ local_interface | ORPHRASE}},  Port ID (outgoing port): {{ remote_interface | ORPHRASE }}
 </group>
 """
 
 def map_neighbors(task: Task, *args, **kwargs) -> Result:
     """Uses CDP to grab neighbor data and return result as dictionary."""
     net_connect = task.host.get_connection('netmiko', task.nornir.config)
-    result_data = net_connect.send_command('show cdp neighbors', **kwargs)
-    parser = ttp(data=result_data,template=cdp_template)
+    result_data = net_connect.send_command('show cdp neighbors detail', **kwargs)
+    parser = ttp(data=result_data,template=cdp_entry_template)
     parser.parse()
     results = parser.result(format='raw')[0][0]
 
-    cdp_data = {}
-    #print(results)
-    for neighbor in results:
-        #print(f"show cdp entry {neighbor}")
-        neighbor_data = net_connect.send_command(f"show cdp entry {neighbor}")
-        entry_parser = ttp(data=neighbor_data, template=cdp_entry_template)
-        entry_parser.parse()
-        cdp_data.update(**entry_parser.result(format='raw')[0][0])
-
     return Result(
-        result=cdp_data,
+        result=results,
         changed=False,
         host=task.host)
